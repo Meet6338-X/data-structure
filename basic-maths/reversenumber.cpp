f5a44e96c49fe13485cc0ba596539acc2292d07e
Meet shah <shahmeet644@gmail.com>
2025-10-01 10:00:34 +0530

    update

--- basic-maths/reversenumber.cpp
+++ basic-maths/reversenumber.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int x;
-    cin>>x;
-	int rev=0;
-        int last=0;
-        while(x>0)
-        {
-            last=x%10;
-            rev = (rev * 10)+last;
-            x=x/10;
-        }
-        cout<<rev;
+    cin >> x;
+    int rev = 0;
+    int last = 0;
+    while (x > 0)
+    {
+        last = x % 10;
+        rev = (rev * 10) + last;
+        x = x / 10;
+    }
+    cout << rev;
 }
