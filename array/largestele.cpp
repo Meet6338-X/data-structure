5d251f120759e7b2db064b558e4c53e940a24a92
Meet6338-X <shahmeet644@gmail.com>
2025-10-25 01:25:03 +0530

    array starter

---
+++ array/largestele.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int largestt(int arr[], int n)
+{
+    int lar = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (lar < arr[i])
+        {
+            lar = arr[i];
+        }
+    }
+    return lar;
+}
+
+int main()
+{
+    int arr[5] = {110, 19, 20, 50, 190};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << largestt(arr, n);
+}
+
+// copy - brute force approch
+//  #include <bits/stdc++.h>
+//  using namespace std;
+
+// int sortArr(vector<int> &arr)
+// {
+//     sort(arr.begin(), arr.end());
+//     return arr[arr.size() - 1];
+// }
+
+// int main()
+// {
+//     vector<int> arr1 = {2, 5, 1, 3, 0};
+//     vector<int> arr2 = {8, 10, 5, 7, 9};
+
+//     cout << "The Largest element in the array is: " << sortArr(arr1) << endl;
+//     cout << "The Largest element in the array is: " << sortArr(arr2);
+
+//     return 0;
+// }
