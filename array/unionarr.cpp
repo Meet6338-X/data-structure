37e48ee475d4a5c0809622de52926939ae6b824b
Meet6338-X <shahmeet644@gmail.com>
2025-10-29 23:03:35 +0530

    updated

--- array/unionarr.cpp
+++ array/unionarr.cpp
@@ -25,6 +25,53 @@ int main()
     unionarr(arr, arr1, n, n1);
 }
 
-void solution3(int arr[], int arr1[], int n, int n1)
-{
-}
+// map
+// copy
+//  vector<int> FindUnion(int arr1[], int arr2[], int n, int m)
+//  {
+//      map<int, int> freq;
+//      vector<int> Union;
+//      for (int i = 0; i < n; i++)
+//          freq[arr1[i]]++;
+//      for (int i = 0; i < m; i++)
+//          freq[arr2[i]]++;
+//      for (auto &it : freq)
+//          Union.push_back(it.first);
+//      return Union;
+//  }
+
+// two pointer method
+// copy
+// vector<int> FindUnion(int arr1[], int arr2[], int n, int m)
+// {
+//     int i = 0, j = 0;  // pointers
+//     vector<int> Union; // Uninon vector
+//     while (i < n && j < m)
+//     {
+//         if (arr1[i] <= arr2[j]) // Case 1 and 2
+//         {
+//             if (Union.size() == 0 || Union.back() != arr1[i])
+//                 Union.push_back(arr1[i]);
+//             i++;
+//         }
+//         else // case 3
+//         {
+//             if (Union.size() == 0 || Union.back() != arr2[j])
+//                 Union.push_back(arr2[j]);
+//             j++;
+//         }
+//     }
+//     while (i < n) // IF any element left in arr1
+//     {
+//         if (Union.back() != arr1[i])
+//             Union.push_back(arr1[i]);
+//         i++;
+//     }
+//     while (j < m) // If any elements left in arr2
+//     {
+//         if (Union.back() != arr2[j])
+//             Union.push_back(arr2[j]);
+//         j++;
+//     }
+//     return Union;
+// }
