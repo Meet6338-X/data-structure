72c706c1b7fe8e4f7b0f2a38c4574f5ebe20e1d3
Meet shah <shahmeet644@gmail.com>
2025-10-29 10:41:20 +0530

    linearsearch

--- array/linearsearch.cpp
+++ array/linearsearch.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 using namespace std;
-
+void linearsearch(int arr[], int n, int k)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == k)
+        {
+            cout << arr[i] << " at index " << i;
+        }
+        else
+        {
+            cout << "element not found";
+            break;
+        }
+    }
+}
 int main()
 {
     int arr[7] = {3, 1, 7, 10, 8, 0, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
-    optimalzero(arr, n);
-    cout << "after array : ";
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i];
-    }
+    linearsearch(arr, n, 111);
 }
