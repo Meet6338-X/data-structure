e645357b8b4d2fd884bac52be05bca7a61600d3b
Meet6338-X <shahmeet644@gmail.com>
2025-10-29 09:41:24 +0530

    updated

--- array/movezeroend.cpp
+++ array/movezeroend.cpp
@@ -18,11 +18,27 @@ void movezero(int arr[], int n)
         index++;
     }
 }
+
+void optimalzero(int arr[], int n)
+{
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+    }
+}
+
 int main()
 {
     int arr[7] = {3, 0, 0, 7, 8, 0, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
-    movezero(arr, n);
+    optimalzero(arr, n);
     cout << "after array : ";
     for (int i = 0; i < n; i++)
     {
