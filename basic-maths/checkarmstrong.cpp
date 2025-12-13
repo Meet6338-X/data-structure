7a9c9df00d0d94fc8050c3284b5c0ce5c2731d17
Meet6338-X <shahmeet644@gmail.com>
2025-12-13 20:47:31 +0530

    CHnages

--- basic-maths/checkarmstrong.cpp
+++ basic-maths/checkarmstrong.cpp
@@ -1,26 +1,30 @@
 #include <cmath>
 #include <iostream>
 using namespace std;
-bool checkArmstrong(int n){
-	int num =n;
-	int count=0;
+bool checkArmstrong(int n)
+{
+	int num = n;
+	int count = 0;
 	int ld;
-	int sum=0;
-	while(num>0){
-		count++;
-		num = num/10; 
-	}
-	num =n;
-	while(num>0)
+	int sum = 0;
+	while (num > 0)
 	{
-		ld = num%10;
-		sum = sum+pow(ld,count);
-		num = num/10;
+		count++;
+		num = num / 10;
 	}
-	if(n==sum){
+	num = n;
+	while (num > 0)
+	{
+		ld = num % 10;
+		sum = sum + pow(ld, count);
+		num = num / 10;
+	}
+	if (n == sum)
+	{
 		return true;
 	}
-	else{
+	else
+	{
 		return false;
 	}
 }
