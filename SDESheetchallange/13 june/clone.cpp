800ea459e697166cdc701dcfe43524a46577fc60
Meet6338-X <shahmeet644@gmail.com>
2026-06-14 00:00:38 +0530

    Day 13

---
+++ SDESheetchallange/13 june/clone.cpp
@@ -0,0 +1,49 @@
+class Solution
+{
+public:
+    Node *copyRandomList(Node *head)
+    {
+        if (!head)
+            return nullptr;
+
+        // Step 1: Insert copied nodes
+        Node *curr = head;
+
+        while (curr)
+        {
+            Node *copy = new Node(curr->val);
+            copy->next = curr->next;
+            curr->next = copy;
+            curr = copy->next;
+        }
+
+        // Step 2: Assign random pointers
+        curr = head;
+
+        while (curr)
+        {
+            if (curr->random)
+                curr->next->random = curr->random->next;
+
+            curr = curr->next->next;
+        }
+
+        // Step 3: Separate the lists
+        Node *dummy = new Node(0);
+        Node *copyCurr = dummy;
+        curr = head;
+
+        while (curr)
+        {
+            Node *copy = curr->next;
+
+            curr->next = copy->next;
+            copyCurr->next = copy;
+
+            copyCurr = copy;
+            curr = curr->next;
+        }
+
+        return dummy->next;
+    }
+};
