0b5227e6a55677f35df846210223678b5dde4ff6
Meet6338-X <shahmeet644@gmail.com>
2026-06-16 23:35:52 +0530

    Day 16

---
+++ SDESheetchallange/16 june/assigncookie.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+class Solution
+{
+public:
+    int findContentChildren(vector<int> &g, vector<int> &s)
+    {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int cp = 0;
+        int cop = 0;
+        while (cp < g.size() && cop < s.size())
+        {
+            if (g[cp] <= s[cop])
+            {
+                cp++;
+            }
+            cop++;
+        }
+        return cp;
+    }
+};
